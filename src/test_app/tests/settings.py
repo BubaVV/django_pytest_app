be8d9f17e8b86e3f120ac46673874940a979939b
Kai Diefenbach <kai.diefenbach@iqpp.de>
2018-03-04 09:35:29 +0100

    Tests in tests sub folder

--- src/test_app/tests/settings.py
+++ src/test_app/tests/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'test_app.test_urls'
+ROOT_URLCONF = 'test_app.tests.urls'
 
 TEMPLATES = [
     {
